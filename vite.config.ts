b6c0b38422819aabe1b4a3c1355a0abf36e92075
Riadz <riad.hachemane@gmail.com>
2022-02-25 20:31:31 +0100

    config tweaks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    Components({
+      dirs: ['src/components', 'src/pages', 'src/layouts'],
+      dts: true,
+    }),
+  ],
+  alias: {
+    '@': path.resolve(__dirname, './src'),
+  },
+});
