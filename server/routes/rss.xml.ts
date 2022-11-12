8e3356320ce7e411a13e63afd2e38080bdf14ac5
Riadz <riad.hachemane@gmail.com>
2022-11-12 18:04:48 +0100

    added rss endpoint

--- server/routes/rss.xml.ts
+++ server/routes/rss.xml.ts
@@ -1,5 +1,43 @@
+import RSS from 'rss'
+import { bucket } from '@/server/cosmic'
+import { BlogType } from '@/services/types/blog'
+
+const BASE_URL = 'https://www.ld-boston.cf'
+
 export default defineEventHandler(async ({ res }) => {
-  const feedString = '<xml-test>hello world</xml-test>'
+  const feed = new RSS({
+    title: 'LD Boston Blog',
+    description:
+      'Hi. My name is Lokmane, but I go by the name L.D. Boston, I’m an author who writes sci-fi thrillers.',
+    site_url: BASE_URL,
+    feed_url: `${BASE_URL}/rss.xml`,
+  })
+
+  try {
+    let posts = (await bucket.getObjects({
+      query: {
+        type: 'posts',
+      },
+      props: 'slug,title,created_at,metadata.description',
+    })) as {
+      objects: BlogType[]
+      total: number
+    }
+
+    for (const post of posts.objects) {
+      feed.item({
+        title: post.title,
+        url: `${BASE_URL}/${post.slug}`,
+        date: post.created_at,
+        description: post.metadata.description,
+      })
+    }
+  } catch {}
+
   res.setHeader('content-type', 'text/xml')
-  res.end(feedString)
+  res.setHeader(
+    'cache-control',
+    'public, maxage=86400, s-maxage=86400, stale-while-revalidate',
+  )
+  res.end(feed.xml({ indent: true }))
 })
